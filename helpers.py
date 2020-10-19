4284dd7ca128990c1d6126f15983c88570c7bcb1
alunacob <adrian.luna.cobos@gmail.com>
2020-10-19 11:01:07 +0000

    continuing structure of postprocessing notebook

--- helpers.py
+++ helpers.py
@@ -102,3 +102,17 @@ def plot_rgb(red,green,blue):
     plt.tight_layout()
     fig = plt.gcf()
     plt.show()
+    
+
+def image_histogram_equalization(image, number_bins=256):
+    # from http://www.janeriksolem.net/2009/06/histogram-equalization-with-python-and.html
+
+    # get image histogram
+    image_histogram, bins = np.histogram(image.flatten(), number_bins, density=True)
+    cdf = image_histogram.cumsum() # cumulative distribution function
+    cdf = 255 * cdf / cdf[-1] # normalize
+
+    # use linear interpolation of cdf to find new pixel values
+    image_equalized = np.interp(image.flatten(), bins[:-1], cdf)
+
+    return image_equalized.reshape(image.shape), cdf
